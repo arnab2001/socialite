e88b24875da06c28225c0430551e7be3402961d7
arnab2001 <63441472+arnab2001@users.noreply.github.com>
2023-04-23 14:07:39 +0530

    fixed posts and friends not rendering 2

--- client/src/scenes/widgets/PostWidget.jsx
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -95,7 +95,7 @@ const PostWidget = ({
       </FlexBetween>
       {isComments && (
         <Box mt="0.5rem">
-          {comments?.map((comment, i) => (
+          {comments?.map(({comment, i}) => (
             <Box key={`${name}-${i}`}>
               <Divider />
               <Typography sx={{ color: main, m: "0.5rem 0", pl: "1rem" }}>
