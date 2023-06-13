07025d20190be4904f05297a3b4be86e4271e13a
arnab2001 <63441472+arnab2001@users.noreply.github.com>
2023-06-13 22:44:22 +0530

    fixed posts showing in wrong order

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -56,12 +56,15 @@ export const createPost = async (req, res) => {
 
 export const getFeedPosts = async (req, res) => {
     try{
-        const post = await Post.find();
+       //fetch by recent post
+        const post = await Post.find().sort({"createdAt": -1});
         res.status(200).json(post);
+        
 
 
     }catch(err){
         res.status(404).json({message: err.message});
+        
     }
 };
 
@@ -69,6 +72,7 @@ export const getUserPosts = async (req, res) => {
     try{
         const { userId } = req.params;
         const post = await Post.find({userId});
+        
         res.status(200).json(post);
         }
     catch(err){
@@ -82,7 +86,7 @@ export const likePost = async (req, res) => {
         const { id} = req.params;
         const { userId } = req.body;
         const post = await Post?.findById(id);
-        console.log(post);
+       
         const isliked = post.likes.get(userId);
         console.log(isliked);
         
